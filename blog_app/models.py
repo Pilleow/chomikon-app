05250af9c49616e62b630c233e0c1358736302d4
Pilleow <pilleowo@gmail.com>
2024-06-29 12:04:50 +0200

    hello

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
+from django.utils import timezone
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,34 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+posts = [
+    {
+        "author": "Enside",
+        "title": "Chomikuj",
+        "content": "Chomik.",
+        "date": "August 16, 2024"
+    },
+    {
+        "author": "Pilleow",
+        "title": "Chomikowanie",
+        "content": "Chomik ale w project zomboid.",
+        "date": "August 15, 2024"
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog_app/home.html", context)
+
+
+def about(request):
+    context = {
+        'title': "About"
+    }
+    return render(request, "blog_app/about.html", context)
